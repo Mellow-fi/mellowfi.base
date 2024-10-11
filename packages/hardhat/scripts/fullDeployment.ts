cbef01f131614787d74676c4d97e4ba10a5b21b1
Katlego R. Phele <113664624+katlegorphele@users.noreply.github.com>
2024-10-11 11:08:52 +0000

    updated full deployment to use USDC

--- packages/hardhat/scripts/fullDeployment.ts
+++ packages/hardhat/scripts/fullDeployment.ts
@@ -9,13 +9,13 @@ async function main() {
 
     // Deploy Collateral Manager
     const Coll = await hre.ethers.getContractFactory("CollateralManager");
-    const coll = await Coll.deploy("0x10c892a6ec43a53e45d0b916b4b7d383b1b78c0f", {});
+    const coll = await Coll.deploy("0x036CbD53842c5426634e7929541eC2318f3dCF7e", {});
     await coll.waitForDeployment();
     logContractDeployed("CollateralManager", coll);
 
     // Deploy Loan Manager
     const Loan = await hre.ethers.getContractFactory("LoanManager");
-    const loan = await Loan.deploy("0x874069fa1eb16d44d622f2e0ca25eea172369bc1", coll.target, {});
+    const loan = await Loan.deploy("0x036CbD53842c5426634e7929541eC2318f3dCF7e", coll.target, {});
     await loan.waitForDeployment();
     logContractDeployed("LoanManager", loan);
 
