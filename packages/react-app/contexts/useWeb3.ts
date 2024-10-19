549f975749b3f339a6cc84f687f886a922b5bb2f
Katlego R. Phele <113664624+katlegorphele@users.noreply.github.com>
2024-10-19 20:53:52 +0000

    updated contracts and added loan repayment functionality. Needs work

--- packages/react-app/contexts/useWeb3.ts
+++ packages/react-app/contexts/useWeb3.ts
@@ -5,49 +5,17 @@ import { useState, useEffect } from "react";
 import StableTokenABI from "./cusd-abi.json";
 import CollateralManagerABI from "./MellowFinanceCollateralManager.json";
 import LoanManagerABI from "./MellowFinanceLoanManager.json"
-import { Address } from "viem";
-import { readContract } from "viem/actions";
+
 
 export const useWeb3 = () => {
   const { address } = useAccount();
-  const COLLATERAL_MANAGER_CONTRACT = "0x0c211c2A104eb0415b33F453f2699265760A5A51";
+  const COLLATERAL_MANAGER_CONTRACT = "0x58E965DCc634f375cD01e695eBe73F1b217ee48c";
   const USDC_CONTRACT_ADDRESS = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
-  const LOAN_MANAGER_CONTRACT = "0x20B3fd5CBd88d4747730DaAFf0872422a4Dbcfc7";
+  const LOAN_MANAGER_CONTRACT = "0xe538ab95d17B7875072D9a6ecC64419484Ec5Ae4";
 
   const {writeContract} = useWriteContract();
 
   
-
-  const getSigner = async () => {
-    if (typeof window !== "undefined" && window.ethereum) {
-      const provider = new BrowserProvider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      return await provider.getSigner();
-    }
-    throw new Error("No Ethereum provider found");
-  };
-
-  interface ExecuteTransactionParams {
-    contractAddress: string;
-    abi: any;
-    method: string;
-    args: any[];
-  }
-
-  const executeTransaction = async ({ contractAddress, abi, method, args }: ExecuteTransactionParams): Promise<any> => {
-    const signer = await getSigner();
-    const contract = new Contract(contractAddress, abi, signer);
-    const tx = await contract[method](...args);
-    await tx.wait();
-    return tx;
-  };
-
-  const executeReadOnly = async ({ contractAddress, abi, method, args }: ExecuteTransactionParams): Promise<any> => {
-    const signer = await getSigner();
-    const contract = new Contract(contractAddress, abi, signer);
-    const result = await contract[method](...args);  // Call the read-only method
-    return result;  // Return the result directly
-  };
 
   const depositNativeCollateral = async (amount: string, func: WriteContractMutate<Config,unknown>) => {
     const amountInWei = parseUnits(amount, 18);
@@ -95,21 +63,6 @@ export const useWeb3 = () => {
     // return await executeTransaction({ contractAddress: COLLATERAL_MANAGER_CONTRACT, abi: CollateralManagerABI.abi, method: "depositStableCollateral", args: [amountInWei] });
   };
 
-  // const getMaxLoanAmount = async () => {
-  //   try{
-  //     const tx = useReadContract({
-  //       abi: LoanManagerABI.abi,
-  //       address: LOAN_MANAGER_CONTRACT,
-  //       functionName: "getMaxLoanAmount",
-  //       args: [address],
-  //     })
-  //     return tx;
-  //   }
-  //   catch (e) {
-  //     console.error(e);
-  //   }
-  // };
-
   
 
   const requestLoan = async (amount: string, func: WriteContractMutate<Config,unknown>) => {
@@ -117,55 +70,56 @@ export const useWeb3 = () => {
 
     try{
 
-      writeContract({
-        abi: StableTokenABI.abi,
-        address: USDC_CONTRACT_ADDRESS,
-        functionName: "approve",
-        args: [COLLATERAL_MANAGER_CONTRACT, amountInWei],
-      });
+      // writeContract({
+      //   abi: StableTokenABI.abi,
+      //   address: USDC_CONTRACT_ADDRESS,
+      //   functionName: "approve",
+      //   args: [COLLATERAL_MANAGER_CONTRACT, amountInWei],
+      // });
 
 
       const tx = writeContract({
         abi: LoanManagerABI.abi,
         address: LOAN_MANAGER_CONTRACT,
         functionName: "requestLoan",
-        args:["1000000","5"]
+        args:[amountInWei.toString()]
       });
+      console.log(tx);
     } catch (e) {
       console.log(e);
     }
   }
 
   const repayLoan = async (amount: string) => {
-    const amountInWei = parseUnits(amount, 18);
-    return await executeTransaction({ contractAddress: LOAN_MANAGER_CONTRACT, abi: LoanManagerABI.abi, method: "repayLoan", args: [amountInWei] });
+    const amountInWei = parseUnits(amount, 6);
+    
+    try{
+
+      writeContract({
+        abi: StableTokenABI.abi,
+        address: USDC_CONTRACT_ADDRESS,
+        functionName: "approve",
+        args: [LOAN_MANAGER_CONTRACT, amountInWei],
+      });
+
+
+      const tx = writeContract({
+        abi: LoanManagerABI.abi,
+        address: LOAN_MANAGER_CONTRACT,
+        functionName: "repayLoan",
+        args:[amountInWei.toString()]
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
-  const getLoanBalancewithInterest = async () => {
-    // return await executeReadOnly({ contractAddress: LOAN_MANAGER_CONTRACT, abi: LoanManagerABI.abi, method: "getLoanBalancewithInterest", args: [address] });
-    return 1
-  }
-
-  const releaseFunds = async () => {
-    return await executeTransaction({ contractAddress: COLLATERAL_MANAGER_CONTRACT, abi: CollateralManagerABI.abi, method: "releaseFunds", args: [] });
-  }
-
-  const signTransaction = async () => {
-    const signer = await getSigner();
-    const res = await signer.signMessage(`Hello from Mellow-Finance!`);
-    console.log("res", res);
-    return res;
-  };
 
   return {
     address,
-    signTransaction,
     depositNativeCollateral,
     depositStableCollateral,
-    // getMaxLoanAmount,
     requestLoan,
     repayLoan,
-    releaseFunds,
-    getLoanBalancewithInterest,
   };
 };
